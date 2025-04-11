d7c836ea17afd9c99dd3ee29564e40655c17bbf9
Abhishek Tripathi <36189721+itstripathi@users.noreply.github.com>
2025-04-11 18:05:30 +0530

    Add src/App.js 1744374930590

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaxonomyTree from './components/TaxonomyTree';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Taxonomy Management</h1>
+            <TaxonomyTree />
+        </div>
+    );
+}
+
+export default App;
